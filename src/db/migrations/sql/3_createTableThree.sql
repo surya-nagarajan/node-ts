fcef8ba35fe704bed1541c0d42197680244300b9
Surya <77264555+surya-nagrajan@users.noreply.github.com>
2022-04-19 12:35:31 +0530

    first commit

---
+++ src/db/migrations/sql/3_createTableThree.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS campaigns_value (
+    ad_id VARCHAR (256),
+    campaign_id VARCHAR (256) PRIMARY KEY,
+    campaign_name VARCHAR (256),
+    campaign_description VARCHAR (256),
+    campaign_category VARCHAR (256),
+    product_name VARCHAR (256),
+    approx_budget VARCHAR (256),
+    campaign_end_date VARCHAR (256),
+    campaign_status VARCHAR (256),
+    campaign_createdAt VARCHAR (256),
+    campaign_reject_reason VARCHAR (256),
+    assigned_orders VARCHAR[] not null default '{}',
+    assigned_influencers VARCHAR[] not null default '{}'
+);
---
+++ src/index.ts
@@ -0,0 +1,64 @@
+import express, { Request, Response } from "express";
+const { Client } = require("pg");
+import path from "path";
+import { migrate } from "postgres-migrations";
+
+const app = express();
+
+const port = 5001;
+
+// const client = new Client({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "postgres",
+//   password: "password",
+//   port: 5432,
+// });
+// client.connect(function (err: any) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
+
+// const migrateFunction=async()=> {
+//   const dbConfig:any = {
+//     database: "database-name",
+//     user: "postgres",
+//     password: "password",
+//     host: "localhost",
+//     port: 5432,
+//     ensureDatabaseExists: true,
+//     defaultDatabase: "postgres"
+//   }
+
+//   await migrate(dbConfig, path.resolve(__dirname, 'db/migrations/sql'));
+//   console.log("inside function after")
+
+// }
+
+const migrateFunction = async () => {
+  const dbConfig = {
+    database: "postgres",
+    user: "postgres",
+    password: "password",
+    host: "localhost",
+    port: 5432,
+  };
+
+  // Note: when passing a client, it is assumed that the database already exists
+  const client = new Client(dbConfig); // or a Pool, or a PoolClient
+  await client.connect();
+  try {
+    await migrate({ client }, path.resolve(__dirname, "db/migrations/sql"));
+  } finally {
+    await client.end();
+  }
+};
+
+app.get("/", (req, res) => {
+  res.send("working");
+});
+
+app.listen(port, () => {
+  console.log(`i am listening on ${port}`);
+  migrateFunction();
+});
